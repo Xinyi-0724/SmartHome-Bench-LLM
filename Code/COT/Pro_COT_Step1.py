f47b3f4aacf0d1098c7eb1eeff8a13da22c4ad5c
Xinyi-0724 <68037622+Xinyi-0724@users.noreply.github.com>
2025-03-25 15:21:37 -0700

    Update Pro_COT_Step1.py

--- Code/COT/Pro_COT_Step1.py
+++ Code/COT/Pro_COT_Step1.py
@@ -6,6 +6,7 @@ import pandas as pd
 import vertexai
 import concurrent.futures
 
+from dotenv import load_dotenv
 from vertexai.generative_models import GenerationConfig, GenerativeModel, Part
 from google.generativeai.types import HarmCategory, HarmBlockThreshold
 from google.cloud.aiplatform_v1beta1.types import (
@@ -13,6 +14,9 @@ from google.cloud.aiplatform_v1beta1.types import (
 )
 
 HarmCategory = gapic_content_types.HarmCategory
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Initialize Vertex AI
 def init_vertex_ai(project_id: str, location="us-central1"):
@@ -138,9 +142,9 @@ def main_part(project_id, model_type, batch_size):
     return jsonl_filename, df_time
 
 # Example usage
-if __name__ == "__main__":
-    #change project_id
-    project_id = 'YourProjectID'
+if __name__ == "__main__": 
+    # Access the project ID from environment variables
+    project_id = os.getenv("PROJECT_ID")
     batch_size = 10  # Adjust batch size as needed
     model_type = 'pro'  # or 'pro'
     filename, df_time = main_part(project_id, model_type, batch_size)
